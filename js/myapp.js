7a89b008cb923e59893e6af95f02117cfa0c7d28
Hokens240 <61212883+Hokens240@users.noreply.github.com>
2026-03-07 08:40:58 +0100

    Adjust account and profit balances for user
    Updated financial values for user account.

--- js/myapp.js
+++ js/myapp.js
@@ -80,11 +80,11 @@ function initializeMockUsers() {
             lastName: "Stanley", 
             country: "United States of America", 
             pass: null, 
-            accountBalance: "13,950.00", 
-            totalProfit: "13,950.00", 
-            profitBalance: "13,950.00", 
+            accountBalance: "16,450.00", 
+            totalProfit: "16,450.00", 
+            profitBalance: "16,450.00", 
             initialInvestment: "1350.00", 
-            returnOnInvestment: "13,950.00",
+            returnOnInvestment: "16,450.00",
             investments: []
         },
     ];
