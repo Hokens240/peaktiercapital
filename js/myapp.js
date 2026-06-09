bad466cd0c9b6411510be1bd60d08f96044c567c
Hokens240 <61212883+Hokens240@users.noreply.github.com>
2026-06-09 11:04:23 +0100

    Update user data

--- js/myapp.js
+++ js/myapp.js
@@ -62,6 +62,19 @@ function initializeMockUsers() {
             investments: []
         },
         { 
+            email: "vunhan008@gmail.com", 
+            firstName: "Stephen", 
+            lastName: " ", 
+            country: "United States of America", 
+            pass: null, 
+            accountBalance: "0.00", 
+            totalProfit: "0.00", 
+            profitBalance: "0.00", 
+            initialInvestment: "0.00", 
+            returnOnInvestment: "0.00",
+            investments: []
+        },
+        { 
             email: "denisjules@gmail.com", 
             firstName: "Denis", 
             lastName: "Jules Guimond", 
